e108c5e0f5d1befa262d58ea3cb7bc6c1568e40a
Andrew Ramirez <alramir3@uci.edu>
2022-10-01 13:05:12 -0700

    more changes

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ function compute() {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal*rate*years;
+    var interest = principal*rate*years/100;
     var year = new Date().getFullYear() + parseInt(years);
-    document.getElementById("result").innerHTML = "If you deposit " + principal + ", at an interest rate of " + "rate" + "%. You will receieve an amount of " + interest + " in the year " + year;
+    document.getElementById("result").innerHTML = "If you deposit $" + principal + ",\<br\>at an interest rate of " + rate + "%.\<br\>You will receieve an amount of " + interest + "\<br\> in the year " + year;
 }
 function updateRate() {
     var ratEval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = ratEval;
+}
+function valCheck() {
+    if(document.getElementById("principal").value <= 0) {
+        alert("Please enter a positive number for Amount")
+    }
+    else {
+        compute()
+    }
 }
         
